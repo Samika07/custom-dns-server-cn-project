e7e1c68eba01a1306b69989a211db6cfeb48b70e
Rohan Komperla <89827166+prohan1287@users.noreply.github.com>
2026-04-17 09:44:23 +0530

    Update test_client.py

--- server/test_client.py
+++ server/test_client.py
@@ -1,15 +1,34 @@
 import socket
 
-server = ("127.0.0.1", 8053)
+SERVER_IP = "127.0.0.1"
+PORT = 8053
 
-# simple DNS query packet
-query = b'\xaa\xaa\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00' \
-        b'\x06google\x05local\x00\x00\x01\x00\x01'
+# Ask user for domain
+domain = input("Enter domain: ")
 
+# Build DNS query packet
+packet = b'\xaa\xaa'          # Transaction ID
+packet += b'\x01\x00'         # Standard query
+packet += b'\x00\x01'         # Questions = 1
+packet += b'\x00\x00'         # Answer RRs
+packet += b'\x00\x00'         # Authority RRs
+packet += b'\x00\x00'         # Additional RRs
+
+# Convert domain to DNS format
+for part in domain.split("."):
+    packet += bytes([len(part)])
+    packet += part.encode()
+
+packet += b'\x00'             # End of domain name
+packet += b'\x00\x01'         # Type A
+packet += b'\x00\x01'         # Class IN
+
+# Send packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(packet, (SERVER_IP, PORT))
 
-sock.sendto(query, server)
-
+# Receive response
 data, _ = sock.recvfrom(512)
 
 print("Response received from DNS server")
+sock.close()
